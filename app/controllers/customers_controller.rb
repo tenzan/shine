1e1c64dc1cdfc1e4579f8d488791e774cb4c8ec9
Askarbek Karasasev <askar.karasaev@gmail.com>
2015-08-12 15:04:05 +0900

    dynamic search UI with angular

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -15,5 +15,11 @@ class CustomersController < ApplicationController
     else
       @customers = []
     end
+
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @customers}
+    end
+
   end
 end
